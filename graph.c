b14d927044d393d4eefe4ec9dec227729300e380
adamnilsson98 <adam@expertocrede.se>
2021-05-10 11:36:50 +0200

    La till alla andra operationer

--- graph.c
+++ graph.c
@@ -25,3 +25,73 @@ graph *graph_empty()
 
   return g;
 }
+
+bool graph_is_empty(const graph *g)
+{
+
+}
+
+bool graph_has_edges(const graph *g)
+{
+
+}
+
+graph *graph_insert_node(graph *g, const char *s)
+{
+
+}
+
+node *graph_find_node(const graph *g, const char *s)
+{
+
+}
+
+bool graph_node_is_seen(const graph *g, const node *n)
+{
+
+}
+
+graph *graph_node_set_seen(graph *g, node *n, bool seen)
+{
+
+}
+
+graph *graph_reset_seen(graph *g)
+{
+
+}
+
+graph *graph_insert_edge(graph *g, node *n1, node *n2)
+{
+
+}
+
+graph *graph_delete_node(graph *g, node *n)
+{
+
+}
+
+graph *graph_delete_edge(graph *g, node *n1, node *n2)
+{
+
+}
+
+node *graph_choose_node(const graph *g)
+{
+
+}
+
+dlist *graph_neighbours(const graph *g,const node *n)
+{
+
+}
+
+void graph_kill(graph *g)
+{
+
+}
+
+void graph_print(const graph *g)
+{
+
+}
