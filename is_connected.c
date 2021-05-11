16821c46e4ab5cd25d777aeb45d96306c83a188b
Sundain <marcussundinn@gmail.com>
2021-05-11 21:04:00 +0200

    Added a breathfirst algorithm.

--- is_connected.c
+++ is_connected.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <graph.h>
+#include <queue.h>
+#include <graph.h>
 
 #define MAXCHAR 1000
 
@@ -40,4 +41,27 @@ int main(void) {
   fclose(fp);
 
   return 0;
+}
+
+graph BreadthFirst(graph g, node n)
+{
+  Queue q = queue_empty(NULL);
+  g=graph_node_is_seen(g, n, true); // (n,g)←seen(n,g) , mark the node as seen.
+  q = queue_enqueue(q,n); // Put node in queue.
+  while (!queue_is_empty(q))
+  {
+    struct node p = queue_front(q); // Pick first node from queue
+    q=queue_dequeue(q); //Remove the first element from the queue
+    struct dlist neighbours=graph_neighbours(g,p);
+
+    dlist_pos pos = dlist_first(neighbours)
+    while(!dlist_is_empty(neighbours)
+    {
+      struct node m = dlist_inspect(neighbours, pos);
+      if (!graph_node_is_seen(g, m))
+      {
+        g=graph_node_is_seen(g, m, true);
+        q = queue_enqueue(q,m);
+      }
+    }
 }
