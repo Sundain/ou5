093ea0a84ddffdcd2a9d2340063b02d40acb03e6
Sundain <marcussundinn@gmail.com>
2021-05-13 18:47:27 +0200

    Fixed small things

--- is_connected.c
+++ is_connected.c
@@ -1,9 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <queue.h>
-#include <graph.h>
+#include "string.h"
+#include "queue.h"
+#include "graph.h"
 
 #define MAXCHAR 1000
 
--- is_connected_file_reader.c
+++ is_connected_file_reader.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "string.h"
 #include "graph.h"
 
 #define MAXCHAR 1000
