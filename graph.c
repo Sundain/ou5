eeccf8c0aa7eaafb8e65a234112b637fae46a46f
adamnilsson98 <adam@expertocrede.se>
2021-05-14 14:40:33 +0200

    Update

--- graph.c
+++ graph.c
@@ -73,7 +73,7 @@ graph *graph_empty(int max_nodes)
  */
 bool graph_is_empty(const graph *g)
 {
-	return g->nmrOfNodes == 0;
+	return dlist_is_empty(g->nodes);
 }
 
 /**
@@ -90,7 +90,7 @@ graph *graph_insert_node(graph *g, const char *s)
 {
 	struct node *n = calloc(1, sizeof(node));
 	n->edges = dlist_empty(NULL);
-	strcpy(n->name, *s);
+	n->name = (char*) s;
 	n->seen = false;
 	g->nmrOfNodes++;
 	dlist_insert(g->nodes, n, dlist_first(g->nodes));
@@ -108,10 +108,9 @@ graph *graph_insert_node(graph *g, const char *s)
 node *graph_find_node(const graph *g, const char *s)
 {
   dlist_pos pos = dlist_first(g->nodes);
-
   while (!dlist_is_end(g->nodes, pos)) {
     struct node *n = dlist_inspect(g->nodes, pos);
-    if (strcmp(n->name, s)) {
+    if (!strcmp(n->name, s)) {
       return n;
     }
     pos = dlist_next(g->nodes, pos);
@@ -202,6 +201,12 @@ dlist *graph_neighbours(const graph *g,const node *n)
  *
  * Returns: Nothing.
  */
+
+char *graph_node_name(const graph *g, const node *n)
+{
+	return n->name;
+}
+
 void graph_kill(graph *g)
 {
 
