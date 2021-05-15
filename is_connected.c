6da2429079ff6e1afec68eeb35d5e88661eee86e
Sundain <marcussundinn@gmail.com>
2021-05-15 12:48:07 +0200

    More changes to algorithm, doesn't quite work yet.

--- is_connected.c
+++ is_connected.c
@@ -1,4 +1,4 @@
-#include <stdbool.h>
+##include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,15 +9,25 @@
 
 #define MAXCHAR 1000
 
-graph *BreadthFirst(graph *g, node *n)
+bool find_path(graph *g, node *src, node *dest)
 {
   queue *q = queue_empty(NULL);
   bool seen = true;
-  graph_node_set_seen(g, n, seen); // (n,g)←seen(n,g) , mark the node as seen.
-  queue_enqueue(q,n); // Put node in queue.
-  while (!queue_is_empty(q))
-  {
 
+  graph_node_set_seen(g, src, seen); // (n,g)←seen(n,g) , mark the node as seen.
+  queue_enqueue(q,src); // Put node in queue.
+
+  //Check if the nodes are the same, returns that the nodes are connected if true.
+  if (nodes_are_equal(dest,src))
+  {
+    printf("12434\n");
+    return true;
+  }
+
+
+  while (!queue_is_empty(q) )
+  {
+    printf("1\n");
     struct node *p = queue_front(q); // Pick first node from queue
     queue_dequeue(q); //Remove the first element from the queue
     struct dlist *neighbours=graph_neighbours(g,p);
@@ -25,35 +35,31 @@ graph *BreadthFirst(graph *g, node *n)
     dlist_pos pos = dlist_first(neighbours);
     while(!dlist_is_end(neighbours,pos))
     {
+      printf("1\n");
       struct node *m = dlist_inspect(neighbours, pos);
+
       if (!graph_node_is_seen(g, m))
       {
-        g=graph_node_set_seen(g, m, seen);
-        q = queue_enqueue(q,m);
+        if (nodes_are_equal(m,dest))
+        {
+          return true;
+        }
+        else
+        {
+          g=graph_node_set_seen(g, m, seen);
+          q = queue_enqueue(q,m);
+        }
       }
       pos = dlist_next(neighbours,pos);
+      //free(m);
     }
+    free(neighbours);
   }
-  return g;
+  return false; //if src not found return false
 }
-
-bool dest_found(graph *g, node *n2)
-{
-  return graph_node_is_seen(g, n2);
-}
-
-bool find_path(graph *g, node *src, node *dest)
-{
-
-  //travsering the graph
-  g=BreadthFirst(g,src);
-  //return if the destination have been marked as seen
-  return dest_found(g, dest);
-}
-
-
 
 int main(void) {
+  //Creating empty graph.
   graph *g = graph_empty(1000);
 
   graph_insert_node(g, "A");
@@ -69,6 +75,7 @@ int main(void) {
   struct node *D = graph_find_node(g,"D");
   struct node *E = graph_find_node(g,"E");
   struct node *F = graph_find_node(g,"F");
+
   graph_insert_edge(g, A, B);
   graph_insert_edge(g, A, C);
   graph_insert_edge(g, C, D);
@@ -76,13 +83,13 @@ int main(void) {
   graph_insert_edge(g, D, A);
   graph_insert_edge(g, E, F);
 
-  struct node *src = E;
-  struct node *dest = F;
-  bool path = find_path(g, src, dest);
+  struct node *src = A;
+  struct node *dest = B;
+  bool connected = find_path(g, src, dest);
 
-  if (path)
+  if (connected)
   {
-    printf("There is no path \n");
+    printf("There is a path \n");
     //printf("There is a path from %s to %s.\n",
     //*graph_node_name(g, src), *graph_node_name(g, dest));
   }
