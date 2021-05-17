c1225999036adc6d3c592fc3996882cca8090082
adamnilsson98 <adam@expertocrede.se>
2021-05-17 11:13:38 +0200

    Complete is_connected

--- is_connected.c
+++ is_connected.c
@@ -21,109 +21,150 @@
  *   2021-05-16: v1.0, first public version.
  */
 
+ bool find_path(graph *g, node *src, node *dest)
+ {
+   //Creating an empty queue
+   queue *q = queue_empty(NULL);
+   bool seen = true;
 
-bool find_path(graph *g, node *src, node *dest)
-{
-  //Creating an empty queue
-  queue *q = queue_empty(NULL);
-  bool seen = true;
+   graph_node_set_seen(g, src, seen); // Mark the "src" node as seen.
+   queue_enqueue(q,src); // Put "src" node in queue.
 
-  graph_node_set_seen(g, src, seen); // Mark the "src" node as seen.
-  queue_enqueue(q,src); // Put "src" node in queue.
-
-  //Check if the nodes are the same, returns that the nodes are connected if true.
-  if (nodes_are_equal(dest,src))
-  {
-    return true;
-  }
+   //Check if the nodes are the same, returns that the nodes are connected if true.
+   if (nodes_are_equal(dest,src))
+   {
+     return true;
+   }
 
 
-  while (!queue_is_empty(q) )
-  {
-    printf("1\n");
-    struct node *p = queue_front(q); // Pick first node from queue
-    queue_dequeue(q); //Remove the first element from the queue
-    struct dlist *neighbours=graph_neighbours(g,p);//saving the neighbours in a directed list.
+   while (!queue_is_empty(q) )
+   {
+     struct node *p = queue_front(q); // Pick first node from queue
+     queue_dequeue(q); //Remove the first element from the queue
+     struct dlist *neighbours=graph_neighbours(g,p);//saving the neighbours in a directed list.
+     dlist_pos pos = dlist_first(neighbours); //checking the position of first element.
+     //Go through neighbours and add to queue if the node is not set to seen.
+     while(!dlist_is_end(neighbours,pos))
+     {
+       char* name = dlist_inspect(neighbours, pos);
+       struct node *m = graph_find_node(g, name);
 
-    dlist_pos pos = dlist_first(neighbours); //checking the position of first element.
-    //Go through neighbours and add to queue if the node is not set to seen.
-    while(!dlist_is_end(neighbours,pos))
-    {
-      printf("1\n");
-      struct node *m = dlist_inspect(neighbours, pos);
+       //Check if the node "m" is set as seen.
+       if (!graph_node_is_seen(g, m))
+       {
+         //Check if the node "m" is the same as "dest",
+         //returns that the nodes are connected if true.
+         if (nodes_are_equal(m,dest))
+         {
+           return true;
+         }
+         //If nodes  are not equal continue with the traversing by setting the node as seen.
+         //The node is also added into the queue.
+         else
+         {
+           g = graph_node_set_seen(g, m, seen); //Set the node "m" as seen.
+           q = queue_enqueue(q,m); //Add the node "m" to the queue.
+         }
+       }
+       pos = dlist_next(neighbours,pos); //Save the position of the next element.
+       //free(m);
+     }
+     free(neighbours);
+   }
+   return false; //if "dest" not found return false
+ }
 
-      //Check if the node "m" is set as seen.
-      if (!graph_node_is_seen(g, m))
-      {
-        //Check if the node "m" is the same as "dest",
-        //returns that the nodes are connected if true.
-        if (nodes_are_equal(m,dest))
-        {
-          return true;
-        }
-        //If nodes  are not equal continue with the traversing by setting the node as seen.
-        //The node is also added into the queue.
-        else
-        {
-          g=graph_node_set_seen(g, m, seen); //Set the node "m" as seen.
-          q = queue_enqueue(q,m); //Add the node "m" to the queue.
-        }
-      }
-      pos = dlist_next(neighbours,pos); //Save the position of the next element.
-      //free(m);
+int main(int argc, char** argv) {
+
+  FILE *fp;
+  char str[MAXCHAR];
+  char* filename = argv[argc-1];
+
+  fp = fopen(filename, "r");
+
+  int i = 0;
+  int number_of_edges = -1;
+
+  while (fgets(str, MAXCHAR, fp) != NULL) {
+    if (str[0] != '#' && str[0] != ' ') {
+      number_of_edges = atoi(str);
+      break;
     }
-    free(neighbours);
   }
-  return false; //if "dest" not found return false
-}
+  char origins[number_of_edges][41];
+  char destinations[number_of_edges][41];
 
-int main(void) {
-  //Creating empty graph.
-  graph *g = graph_empty(1000);
+  while (fgets(str, MAXCHAR, fp) != NULL) {
+    if (str[0] != '#' && str[0] != ' ') {
+      char sub[41];
+      int j = 0;
+      while(str[j] != ' ') {
+        sub[j] = str[j];
+        j++;
+      }
+      strcpy(origins[i], sub);
+      memset(sub, 0, strlen(sub));
 
-  graph_insert_node(g, "A");
-  graph_insert_node(g, "B");
-  graph_insert_node(g, "C");
-  graph_insert_node(g, "D");
-  graph_insert_node(g, "E");
-  graph_insert_node(g, "F");
+      j++;
+      int k = 0;
+      while(str[j] != ' ' && str[j] != '#' && str[j] != '\n') {
+        sub[k] = str[j];
+        j++;
+        k++;
+      }
+      strcpy(destinations[i], sub);
+      memset(sub, 0, strlen(sub));
+      i++;
+    }
+  }
 
-  struct node *A = graph_find_node(g,"A");
-  struct node *B = graph_find_node(g,"B");
-  struct node *C = graph_find_node(g,"C");
-  struct node *D = graph_find_node(g,"D");
-  struct node *E = graph_find_node(g,"E");
-  struct node *F = graph_find_node(g,"F");
+  fclose(fp);
 
-  graph_insert_edge(g, A, B);
-  graph_insert_edge(g, A, C);
-  graph_insert_edge(g, C, D);
-  graph_insert_edge(g, B, C);
-  graph_insert_edge(g, D, A);
-  graph_insert_edge(g, E, F);
+  graph *g = graph_empty(10);
 
-  struct node *src = A;
-  struct node *dest = B;
+  for (size_t i = 0; i < number_of_edges; i++) {
+    if (graph_find_node(g, origins[i]) == NULL) {
+      graph_insert_node(g, origins[i]);
+    }
+  }
+
+  for (size_t i = 0; i < number_of_edges; i++) {
+    if (graph_find_node(g, destinations[i]) == NULL) {
+      graph_insert_node(g, destinations[i]);
+    }
+  }
+
+  for (size_t i = 0; i < number_of_edges; i++) {
+    struct node *n1 = graph_find_node(g, origins[i]);
+    struct node *n2 = graph_find_node(g, destinations[i]);
+    graph_insert_edge(g, n1, n2);
+  }
+
+  char src_name[41];
+  char dest_name[41];
+
+  printf("Enter origin and destination (quit to exit): ");
+  scanf("%s %s", src_name, dest_name);
+
+  struct node *src = graph_find_node(g, src_name);
+  struct node *dest = graph_find_node(g, dest_name);
+
   bool connected = find_path(g, src, dest);
 
   if (connected)
   {
-    printf("There is a path \n");
+    printf("There is a path from %s to %s. \n", src_name, dest_name);
     //printf("There is a path from %s to %s.\n",
     //*graph_node_name(g, src), *graph_node_name(g, dest));
   }
   else
   {
-    printf("There is no path \n");
+    printf("There is no path from %s to %s. \n", src_name, dest_name);
   }
 
-    //reset seen status between runs:
-    g=graph_reset_seen(g);
+  //reset seen status between runs:
+  g = graph_reset_seen(g);
 
-    printf("Normal exit.\n");
-    return 0;
+  printf("Normal exit.\n");
+  return 0;
 }
-//Finns slide 38/104 bredden-först-travisering, sida 110
-//Check if destination and source is seen.
-//Graph reset: Use graph_reset_seen
-//Hur avallokeras dynamiskt minne?
