645b456372fcab145db335b3ff7382a5df92bbf9
adamnilsson98 <adam@expertocrede.se>
2021-05-20 16:44:05 +0200

    commented

--- is_connected.c
+++ is_connected.c
@@ -72,11 +72,8 @@
            q = queue_enqueue(q,m); //Add the node "m" to the queue.
          }
        }
-       //dlist_pos pos_temp=pos;
        pos = dlist_next(neighbours,pos); //Save the position of the next element.
-       //dlist_remove(neighbours,pos_temp);
      }
-     //dlist_kill(neighbours);
    }
    queue_kill(q);
 
@@ -89,35 +86,42 @@ int main(int argc, char** argv) {
   char str[MAXCHAR];
   char* filename = argv[argc-1];
 
-  fp = fopen(filename, "r");
+  fp = fopen(filename, "r");  //Open mapfile
 
   int i = 0;
   int number_of_edges = -1;
-  bool error = false;
+  bool error = false;         //Checks if everything works correctly
 
+  //loops until it sees a number
   while (fgets(str, MAXCHAR, fp) != NULL) {
+    //ignors if the row starts with a # or blanck space
     if (str[0] != '#' && str[0] != ' ') {
       number_of_edges = atoi(str);
       break;
     }
   }
+  //checks if number_of_edges have been given a value
   if (number_of_edges < 1)
   {
     error = true;
   }
 
+  //string vector containing nodes connected by edges
   char origins[number_of_edges][41];
   char destinations[number_of_edges][41];
 
+  //loops over the rest of the file
   while (fgets(str, MAXCHAR, fp) != NULL && !error) {
+    //ignors rows starting with # or blanck space
     if (str[0] != '#' && str[0] != ' ')
     {
-      char sub[41];
+      char sub[41]; //name holder variable
       int j = 0;
+      //reads the name until it hits a blanck space
       while(str[j] != ' ' && !error)
       {
         sub[j] = str[j];
-        if (str[j] == '\n')
+        if (str[j] == '\n') //error if it hits a new row already
           error = true;
         j++;
       }
@@ -125,25 +129,27 @@ int main(int argc, char** argv) {
         break;
       }
 
-      strcpy(origins[i], sub);
+      strcpy(origins[i], sub);    //puts in origin name
       memset(sub, 0, strlen(sub));
 
       j++;
       int k = 0;
+      //reads the name until it ends
       while(str[j] != ' ' && str[j] != '#' && str[j] != '\n') {
         sub[k] = str[j];
         j++;
         k++;
       }
-      strcpy(destinations[i], sub);
+      strcpy(destinations[i], sub); //puts in the new destination name
       memset(sub, 0, strlen(sub));
       i++;
     }
   }
-
+  //checks that the indicated number of edges is correct
   if (i != number_of_edges)
     error = true;
 
+  //error message
   if (error) {
     printf("Invalid map\n");
     exit(EXIT_FAILURE);
@@ -151,20 +157,21 @@ int main(int argc, char** argv) {
 
   fclose(fp);
 
-  graph *g = graph_empty(10);
+  graph *g = graph_empty(10); //empty graph
 
+  //inserts the origin nodes
   for (size_t i = 0; i < number_of_edges; i++) {
     if (graph_find_node(g, origins[i]) == NULL) {
       graph_insert_node(g, origins[i]);
     }
   }
-
+  //inserts the destination nodes
   for (size_t i = 0; i < number_of_edges; i++) {
     if (graph_find_node(g, destinations[i]) == NULL) {
       graph_insert_node(g, destinations[i]);
     }
   }
-
+  //inserts the edges
   for (size_t i = 0; i < number_of_edges; i++) {
     struct node *n1 = graph_find_node(g, origins[i]);
     struct node *n2 = graph_find_node(g, destinations[i]);
@@ -178,11 +185,12 @@ int main(int argc, char** argv) {
     char src_name[41];
     char dest_name[41];
     char names[2][41];
-    error = false;
-
+    error = false;  //reset error
+    //asks for node names
     printf("Enter origin and destination (quit to exit): ");
     for (int i = 0; i < 2; i++) {
       scanf("%s", names[i]);
+      //checks if user has entered "quit"
       if (!strcmp(names[0], "quit")) {
         loop = false;
         break;
@@ -193,7 +201,7 @@ int main(int argc, char** argv) {
     {
       strcpy(src_name, names[0]);
       strcpy(dest_name, names[1]);
-
+      //checks if nodes exist
       if (graph_find_node(g, src_name) == NULL ||
           graph_find_node(g, dest_name) == NULL)
           error = true;
@@ -202,12 +210,11 @@ int main(int argc, char** argv) {
         struct node *src = graph_find_node(g, src_name);
         struct node *dest = graph_find_node(g, dest_name);
 
-        bool connected = find_path(g, src, dest);
+        bool connected = find_path(g, src, dest); //try to find a path
+        //message
         if (connected)
         {
           printf("There is a path from %s to %s. \n", src_name, dest_name);
-          //printf("There is a path from %s to %s.\n",
-          //*graph_node_name(g, src), *graph_node_name(g, dest));
         }
         else
         {
@@ -220,7 +227,7 @@ int main(int argc, char** argv) {
         printf("Invalid input\n");
     }
   }
-  graph_kill(g);
+  graph_kill(g);  //kill the graph
   printf("Normal exit.\n");
   return 0;
 }
